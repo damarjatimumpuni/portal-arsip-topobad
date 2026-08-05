cfbc6d6ae5db1bf097b295ef48776c88d4780b55
Damar <damarjatimumpuni153@gmail.com>
2026-08-05 15:53:36 +0700

    Pasang adaptor Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,17 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import vercel from '@astrojs/vercel';
+
 // [https://astro.build/config](https://astro.build/config)
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   // Perhatikan tanda koma di ujung baris bawah ini:
   integrations: [react(), markdoc(), keystatic()],
-  output: 'static'
+
+  output: 'static',
+  adapter: vercel()
 });
